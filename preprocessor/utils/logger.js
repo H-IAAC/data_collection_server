eb63fb3753d5d26857b6700718f06ca61acd2dd1
Saulo A. Moraes <saulo.moraes@eldorado.org.br>
2023-01-13 11:42:32 -0300

    Fix log format to show timestamp first

--- preprocessor/utils/logger.js
+++ preprocessor/utils/logger.js
@@ -1,20 +1,27 @@
 const winston = require('winston');
- 
+
+var logFormat = winston.format.combine(
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SSS' }),
+    winston.format.printf((info) => {
+        return JSON.stringify({
+            TIMESTAMP: info.timestamp,
+            LEVEL: info.level,
+            LOG: info.message
+        });
+    }));
+
 const logger = winston.createLogger({
-    format: winston.format.combine(
-        winston.format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
-        winston.format.prettyPrint()
-    ),
+    format: logFormat,
     transports: [
         new winston.transports.File({ filename: './logs/error.log', level: 'error' }),
         new winston.transports.File({ filename: './logs/info.log', level: 'info' }),
     ],
 });
- 
+
 if (process.env.NODE_ENV !== 'production') {
     logger.add(new winston.transports.Console({
-        format: winston.format.simple()
+        format: logFormat
     }));
 }
- 
+
 module.exports = logger;
