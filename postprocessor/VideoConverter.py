997bfefdcf08cb88485dc6254465c999ef8b633e
Saulo A. Moraes <saulo.moraes@eldorado.org.br>
2024-04-04 13:13:18 -0300

    Remove face_recognition dependency

--- postprocessor/VideoConverter.py
+++ postprocessor/VideoConverter.py
@@ -2,57 +2,57 @@ import cv2
 import mediapipe as mp
 import ffmpegcv
 from Logger import Logger
-import face_recognition #https://github.com/ageitgey/face_recognition
+#import face_recognition #https://github.com/ageitgey/face_recognition
 import cv2
 import moviepy.editor as mpe
 
 class VideoConverter:
-    @staticmethod
-    def hide_faces_using_face_recognition(video_in, video_out):
-        
-        # Get a reference to webcam #0 (the default one)
-        video_capture = cv2.VideoCapture(video_in)
-        
-        video_fps = int(video_capture.get(cv2.CAP_PROP_FPS))
-        video_output = ffmpegcv.VideoWriter(video_out, None, video_fps)
+    #@staticmethod
+    #def hide_faces_using_face_recognition(video_in, video_out):
+    #    
+    #    # Get a reference to webcam #0 (the default one)
+    #    video_capture = cv2.VideoCapture(video_in)
+    #    
+    #    video_fps = int(video_capture.get(cv2.CAP_PROP_FPS))
+    #    video_output = ffmpegcv.VideoWriter(video_out, None, video_fps)
 
         # Initialize some variables
-        face_locations = []
+    #    face_locations = []
 
-        while True:
+    #    while True:
             # Grab a single frame of video
-            ret, frame = video_capture.read()
+    #        ret, frame = video_capture.read()
 
-            if ret == True:
+    #        if ret == True:
                 # Resize frame of video to 1/4 size for faster face detection processing
-                small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    #            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
                 # Find all the faces and face encodings in the current frame of video
-                face_locations = face_recognition.face_locations(small_frame, model="cnn")
+    #            face_locations = face_recognition.face_locations(small_frame, model="cnn")
 
                 # Display the results
-                for top, right, bottom, left in face_locations:
+    #            for top, right, bottom, left in face_locations:
                     # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-                    top *= 4
-                    right *= 4
-                    bottom *= 4
-                    left *= 4
+    #                top *= 4
+    #                right *= 4
+    #                bottom *= 4
+    #                left *= 4
                     # Extract the region of the image that contains the face
-                    face_image = frame[top:bottom, left:right]
+    #                face_image = frame[top:bottom, left:right]
                     # Blur the face image
-                    face_image = cv2.GaussianBlur(face_image, (99, 99), 30)
+    #                face_image = cv2.GaussianBlur(face_image, (99, 99), 30)
                     # Put the blurred face region back into the frame image
-                    frame[top:bottom, left:right] = face_image
+    #                frame[top:bottom, left:right] = face_image
                 # Display the resulting image
                 #cv2.imshow('Video', frame)
-                video_output.write(frame)
+    #            video_output.write(frame)
 
-            else:
-                break
+    #        else:
+    #            break
 
         # Release handle to the webcam
-        video_capture.release()
-        video_output.release()
+    #    video_capture.release()
+    #    video_output.release()
     
     @staticmethod
     def hide_faces_using_mediapipe(video_in, video_out):
